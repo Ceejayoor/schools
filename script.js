5757c23b44ef6ad43d36295d6e9914ab3a55a524
Chijioke <154600443+Ceejayoor@users.noreply.github.com>
2025-05-31 09:47:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const paragraph = document.getElementById("paragraph");
 
 submit.onclick = function(){
     if(userName.value === "stephen" && password.value === "stephen123"){
-        paragraph.innerHTML = "https://sites.google.com/view/bedrockcollegeschool/jss1/stephen-jaiyesimi";
+        window.location.href = "https://sites.google.com/view/bedrockcollegeschool/jss1/stephen-jaiyesimi";
     }
     else if(userName.value === "rita" && password.value === "rita123"){
-        paragraph.innerHTML = "https://sites.google.com/view/bedrockcollegeschool/jss1/rita-sina";
+       window.location.href = "https://sites.google.com/view/bedrockcollegeschool/jss1/rita-sina";
     }
     else if(userName.value === "emeka" && password.value === "emeka123"){
-        paragraph.innerHTML = "https://sites.google.com/view/bedrockcollegeschool/jss1/emeka-azikiwe";
+        window.location.href = "https://sites.google.com/view/bedrockcollegeschool/jss1/emeka-azikiwe";
     }
 }
