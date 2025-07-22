2e104b4bed0cf6d74c2aca082f0cb2f5434bb7ad
Chijioke <154600443+Ceejayoor@users.noreply.github.com>
2025-07-22 13:44:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const submit = document.getElementById("submit");
 const paragraph = document.getElementById("paragraph");
 
 submit.onclick = function(){
-    if(userName.value === "Stephen" && password.value === "Stephen123"){
+    if(userName.value === "Ogeleka" && password.value === "Ogeleka123"){
         window.location.href = "https://sites.google.com/view/donumchristischool/college/jss1/ax-student";
     }
-    else if(userName.value === "Rita" && password.value === "Rita123"){
+    else if(userName.value === "David" && password.value === "David123"){
        window.location.href = "https://sites.google.com/view/donumchristischool/college/jss1/ay-student";
     }
-    else if(userName.value === "Emeka" && password.value === "Emeka123"){
+    else if(userName.value === "Peculiar" && password.value === "Peculiar123"){
         window.location.href = "https://sites.google.com/view/donumchristischool/college/jss1/az-student";
     }
 }
